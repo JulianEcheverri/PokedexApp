6f7e1f7c19e4eb4d0e0f5545680008a72c9759b2
JulianEcheverri <julianecheverri@outlook.com>
2019-11-26 12:05:30 -0500

    Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 import { HomeComponent } from './components/home/home.component';
 import { PokedexService } from './services/pokedex.service';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonComponent,
-    HeaderComponent,
     FooterComponent,
     PokemonListComponent,
     PokemonDetailComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
